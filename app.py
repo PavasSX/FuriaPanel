197a63c98b124f3324551cc931d5edbe4940faa6
PavasSX <159845337+PavasSX@users.noreply.github.com>
2025-05-04 19:35:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -175,4 +175,4 @@ def editar_estatisticas():
     return render_template("estatisticas.html", estatisticas=estatisticas)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
